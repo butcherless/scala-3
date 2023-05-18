04a33290099ddc7c2b9a4b953b05b73f09fd4ce9
Carlos Martin <cmarting@gmail.com>
2023-05-18 10:22:16 +0200

    * upgrade prelude

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   // main
   val zio        = "2.0.13"
   val zioLogging = "2.1.12"
-  val zioPrelude = "1.0.0-RC16"
+  val zioPrelude = "1.0.0-RC19"
 
   // test
   val scalatest = "3.2.16"
