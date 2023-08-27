214d9dc574b4f20fb2b7e4ef52201da4d1e28599
Carlos Martin <cmarting@gmail.com>
2023-08-27 11:30:32 +0200

    review gh actions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,9 +2,6 @@ import sbt._
 
 object Dependencies {
 
-  // production code
-  // lazy val logback = "ch.qos.logback" % "logback-classic" % Versions.logback
-
   // ZIO and ecosystem
   lazy val zio        = "dev.zio" %% "zio"               % Versions.zio
   lazy val zioLogging = "dev.zio" %% "zio-logging-slf4j" % Versions.zioLogging
