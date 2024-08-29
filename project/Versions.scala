068e4ac1493e6c46ce6b7fe4d262206890acd1ec
Carlos Martin <cmarting@gmail.com>
2024-08-29 13:12:31 +0200

    upgrade dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,9 +1,9 @@
 object Versions {
   val scala      = "3.5.0"
   // main
-  val zio        = "2.1.7"
-  val zioLogging = "2.3.0"
-  val zioPrelude = "1.0.0-RC28"
+  val zio        = "2.1.9"
+  val zioLogging = "2.3.1"
+  val zioPrelude = "1.0.0-RC30"
 
   // test
   val scalatest = "3.2.19"
