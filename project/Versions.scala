cddaf5ca5a52a54c980830fe5f20f9163c1f230e
Carlos Martin <cmarting@gmail.com>
2024-12-28 23:21:28 +0100

    upgrade deps

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   // main
   val zio        = "2.1.14"
   val zioLogging = "2.4.0"
-  val zioPrelude = "1.0.0-RC35"
+  val zioPrelude = "1.0.0-RC36"
 
   // test
   val scalatest = "3.2.19"
