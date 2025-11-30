a399eb141eea5b65b982a3be8c5352677ec8579e
Carlos Martin <cmarting@gmail.com>
2025-11-30 14:51:39 +0100

    - upgrade versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,8 +1,8 @@
 object Versions {
   val scala      = "3.3.7"
   // main
-  val zio        = "2.1.22"
-  val zioLogging = "2.5.1"
+  val zio        = "2.1.23"
+  val zioLogging = "2.5.2"
   val zioPrelude = "1.0.0-RC42"
 
   // test
